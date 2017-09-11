7e8d98dca93007d4c799c90090abeeac9f0c3d26
Henric Lindén <henric.linden@rt-labs.com>
2017-09-11 09:51:02 +0200

    Clean up. Moved hci dump file location from tmp to local directory.

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /*
- * Generic main for Bluetooth LE apps.
- * Currently tailored for use with USB HCI dongles.
+ * main for Bluetooth LE apps.
+ * For use with USB HCI dongles.
  *
  * Henric Lindén, rt-labs AB
  */
@@ -27,22 +27,44 @@ int btstack_main(int argc, const char* argv[]);
 
 static btstack_packet_callback_registration_t hci_event_callback_registration;
 
-static void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t* packet, uint16_t size)
+// A HCI event handler informing about the current state. Not needed
+// for normal operation.
+static void hci_packet_handler(uint8_t packet_type, uint16_t channel, uint8_t* packet, uint16_t size)
 {
     UNUSED(channel);
     UNUSED(size);
 
-    if (packet_type != HCI_EVENT_PACKET) {
-        return;
+    if (packet_type == HCI_EVENT_PACKET) {
+        switch (hci_event_packet_get_type(packet)) {
+            case BTSTACK_EVENT_STATE:
+                switch (btstack_event_state_get_state(packet)) {
+                    case HCI_STATE_OFF:
+                        printf("[HCI_STATE_OFF]\n");
+                        break;
+                    case HCI_STATE_INITIALIZING:
+                        printf("[HCI_STATE_INITIALIZING]\n");
+                        break;
+                    case HCI_STATE_WORKING:
+                        printf("[HCI_STATE_WORKING]\n");
+                        break;
+                    case HCI_STATE_HALTING:
+                        printf("[HCI_STATE_HALTING]\n");
+                        break;
+                    case HCI_STATE_SLEEPING:
+                        printf("[HCI_STATE_SLEEPING]\n");
+                        break;
+                    case HCI_STATE_FALLING_ASLEEP:
+                        printf("[HCI_STATE_FALLING_ASLEEP]\n");
+                        break;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            default:
+                break;
+        }
     }
-    if (hci_event_packet_get_type(packet) != BTSTACK_EVENT_STATE) {
-        return;
-    }
-    if (btstack_event_state_get_state(packet) != HCI_STATE_WORKING) {
-        return;
-    }
-
-    printf("BTstack up and running.\n");
 }
 
 static void sigint_handler(int param)
@@ -65,56 +87,19 @@ static void sigint_handler(int param)
 #define USB_MAX_PATH_LEN 7
 int main(int argc, const char* argv[])
 {
-    uint8_t usb_path[USB_MAX_PATH_LEN];
-    int usb_path_len = 0;
-
-    // Parse command line options for USB port string ("-u 11:22:33")
-    if (argc >= 3 && strcmp(argv[1], "-u") == 0) {
-        const char* port_str = argv[2];
-        printf("Specified USB Path: ");
-        while (1) {
-            char* delimiter;
-            int port = strtol(port_str, &delimiter, 16);
-            usb_path[usb_path_len] = port;
-            usb_path_len++;
-            printf("%02x ", port);
-            if (!delimiter) {
-                break;
-            }
-            if (*delimiter != ':' && *delimiter != '-') {
-                break;
-            }
-            port_str = delimiter + 1;
-        }
-        printf("\n");
-        argc -= 2;
-        memmove(&argv[0], &argv[2], argc * sizeof(char*));
-    }
-
     // Do necessary stack initialisations.
     btstack_memory_init();
     btstack_run_loop_init(btstack_run_loop_posix_get_instance());
 
-    if (usb_path_len) {
-        hci_transport_usb_set_path(usb_path_len, usb_path);
-    }
-
-    // Use logger: format HCI_DUMP_PACKETLOGGER, HCI_DUMP_BLUEZ or HCI_DUMP_STDOUT
-    char pklg_path[100];
-    strcpy(pklg_path, "/tmp/hci_dump");
-    if (usb_path_len) {
-        strcat(pklg_path, "_");
-        strcat(pklg_path, argv[2]);
-    }
-    strcat(pklg_path, ".pklg");
-    printf("Packet Log: %s\n", pklg_path);
-    hci_dump_open(pklg_path, HCI_DUMP_PACKETLOGGER);
+    // Redirect log to packetlogger for use in Wireshark.
+    hci_dump_open("hci_dump.pklg", HCI_DUMP_PACKETLOGGER);
+    log_info("Program name: %s", argv[0]);
 
     // Init HCI
     hci_init(hci_transport_usb_instance(), NULL);
 
-    // Inform about BTstack state
-    hci_event_callback_registration.callback = &packet_handler;
+    // Add a hci event handler just for showing btstatck state to user.
+    hci_event_callback_registration.callback = &hci_packet_handler;
     hci_add_event_handler(&hci_event_callback_registration);
 
     // Handle CTRL-C
@@ -123,7 +108,7 @@ int main(int argc, const char* argv[])
     // Setup app
     btstack_main(argc, argv);
 
-    // Do runloop
+    // Execute runloop
     btstack_run_loop_execute();
 
     // Shouldn't end up here...
