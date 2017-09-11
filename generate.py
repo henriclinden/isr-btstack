33e69b8f06459ed1191f5dce3d4afc1b4abc678e
Henric Lindén <henric.linden@rt-labs.com>
2017-09-11 08:57:17 +0200

    Generator updated with simple stats.

--- generate.py
+++ generate.py
@@ -8,28 +8,35 @@ from serial import Serial
 from time import time
 from time import sleep
 from sys import argv
-from sys import stdout
 
-def lprint(string):
-    stdout.write(string)
-    stdout.flush()
 
-if __name__ == "__main__":
-    serialport = argv[1]
-    sp = Serial(serialport, baudrate=57600, rtscts=True, timeout=1.0)
+if len(argv) != 2:
+	print("usage: {} serialport".format(argv[0]))
+	exit(1)
 
-    data = "The quick brown fox jumps over the lazy dog" * 50
+serialport = argv[1]
+sp = Serial(serialport, baudrate=57600, rtscts=True, timeout=1.0)
+sp.flushInput()
+sp.flushOutput()
+sleep(0.1)
 
-    for length in [10, 20, 30, 50, 100, 200, 300, 400, 500]:
-        print "Length", length, "x10"
-        for i in range(10):
-            lprint("<")
-            req = data[:length - 1] + '\n'
-            sp.write(req)
-            sp.flush()
-            resp = sp.read(length)
-            if len(resp) == len(req) and resp == req:
-                lprint(">")
-            else:
-                lprint("!")
-        print ""
+data = "The quick brown fox jumps over the lazy dog" * 50
+
+for length in [10, 20, 30, 50, 100, 200, 300, 400, 500]:
+	for i in range(10):
+		req = data[:length - 1] + '\n'
+		t1 = time()
+		sp.write(req)
+		sp.flush()
+		d1 = time() - t1
+		t2 = time()
+		resp = sp.read(length)
+		d2 = time() - t2
+		if len(resp) == len(req) and resp == req:
+			print("{0:3d}: {1:0.5f} {2:0.5f} {3:0.5f} {4:0.5f}".format(length, d1, d2, length/d1, length/d2))
+		else:
+			print("Mismatch. Got {} bytes of {}".format(len(resp), len(req)))
+			sp.flushInput()
+			sp.flushOutput()
+			sleep(0.1)
+	print("")
