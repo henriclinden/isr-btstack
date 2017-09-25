d196eddbc551ccc12497bd9c8bf890e791ae9575
Henric Lindén <henric.linden@rt-labs.com>
2017-09-25 21:55:26 +0200

    Added patch files for WL1273 (odin-w160)

--- main_uart.c
+++ main_uart.c
@@ -19,13 +19,26 @@
 #include "btstack_run_loop.h"
 #include "btstack_run_loop_posix.h"
 
+#include "bluetooth_company_id.h"
+
 #include "hci.h"
 #include "hci_dump.h"
 #include "btstack_stdin.h"
 
+#include "btstack_chipset_cc256x.h"
+
+
 int btstack_main(int argc, const char* argv[]);
 
 static btstack_packet_callback_registration_t hci_event_callback_registration;
+
+static hci_transport_config_uart_t config = {
+    HCI_TRANSPORT_CONFIG_UART,
+    115200,         // Default baudrate after a reset of WL1273
+    0,              // Main baudrate
+    1,              // Flow control
+    "/dev/ttyO5"    // Bluetooth on ELIN (ODIN-W160)
+};
 
 // A HCI event handler informing about the current state. Not needed
 // for normal operation.
@@ -61,6 +74,35 @@ static void hci_packet_handler(uint8_t packet_type, uint16_t channel, uint8_t* p
                         break;
                 }
                 break;
+            case HCI_EVENT_COMMAND_COMPLETE:
+                if (HCI_EVENT_IS_COMMAND_COMPLETE(packet, hci_read_local_name)) {
+                    if (hci_event_command_complete_get_return_parameters(packet)[0]) {
+                        break;
+                    }
+                    packet[6 + 248] = '\0';
+                    printf("Local name: %.247s<-->\n", &packet[6]);
+                }
+                if (HCI_EVENT_IS_COMMAND_COMPLETE(packet, hci_read_local_version_information)) {
+                    printf("Local version information:\n");
+                    uint16_t hci_version    = packet[6];
+                    uint16_t hci_revision   = little_endian_read_16(packet, 7);
+                    uint16_t lmp_version    = packet[9];
+                    uint16_t manufacturer   = little_endian_read_16(packet, 10);
+                    uint16_t lmp_subversion = little_endian_read_16(packet, 12);
+                    printf("- HCI Version    0x%04x\n", hci_version);
+                    printf("- HCI Revision   0x%04x\n", hci_revision);
+                    printf("- LMP Version    0x%04x\n", lmp_version);
+                    printf("- LMP Subversion 0x%04x\n", lmp_subversion);
+                    printf("- Manufacturer   0x%04x\n", manufacturer);
+                    if (manufacturer == BLUETOOTH_COMPANY_ID_TEXAS_INSTRUMENTS_INC) {
+                        printf("Texas Instruments - CC256x compatible chipset.\n");
+                        hci_set_chipset(btstack_chipset_cc256x_instance());
+                    }
+                    else {
+                        printf("This app is made for the ELIN+ODIN-W160 combo.");
+                    }
+                }
+                break;
             default:
                 break;
         }
@@ -86,14 +128,6 @@ static void sigint_handler(int param)
 
 int main(int argc, const char* argv[])
 {
-    hci_transport_config_uart_t config = {
-        HCI_TRANSPORT_CONFIG_UART,
-        115200,
-        0,  // main baudrate
-        1,  // flow control
-        NULL,
-    };
-
     // Do necessary stack initialisations.
     btstack_memory_init();
     btstack_run_loop_init(btstack_run_loop_posix_get_instance());
@@ -103,10 +137,10 @@ int main(int argc, const char* argv[])
     log_info("Program name: %s", argv[0]);
 
     // Init HCI.
-    const btstack_uart_block_t * uart_driver = btstack_uart_block_posix_instance();
-	const hci_transport_t * transport = hci_transport_h4_instance(uart_driver);
+    const btstack_uart_block_t* uart_driver = btstack_uart_block_posix_instance();
+    const hci_transport_t* transport = hci_transport_h4_instance(uart_driver);
     //const btstack_link_key_db_t * link_key_db = btstack_link_key_db_fs_instance();
-	hci_init(transport, (void*) &config);
+    hci_init(transport, (void*) &config);
     //hci_set_link_key_db(link_key_db);
 
     // Add a hci event handler just for showing btstatck state to user.
